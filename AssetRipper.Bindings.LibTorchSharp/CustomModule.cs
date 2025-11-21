b77326c74f12a4e460f7c0c114557734913208b7
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-11-20 18:50:39 -0800

    Add simple constructor to CustomModule

---
+++ AssetRipper.Bindings.LibTorchSharp/CustomModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace AssetRipper.Bindings.LibTorchSharp;
+
+public readonly partial struct CustomModule
+{
+	public unsafe CustomModule(NativeString name) : this(name, &TrivialForwardMethod)
+	{
+	}
+
+	[UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+	private static Tensor TrivialForwardMethod(Tensor tensor)
+	{
+		return tensor;
+	}
+}
