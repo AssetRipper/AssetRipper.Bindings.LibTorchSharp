70c1a88cf79e781c1b735bc0f05819bf3d980bd1
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-05 19:34:50 -0800

    Tensor data access

---
+++ AssetRipper.Bindings.LibTorchSharp/Tensor.Data.cs
@@ -0,0 +1,342 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AssetRipper.Bindings.LibTorchSharp;
+
+public readonly partial struct Tensor
+{
+	public T[] ToArray<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(1);
+		return ToArray1D_Internal<T>();
+	}
+
+	private unsafe T[] ToArray1D_Internal<T>() where T : unmanaged
+	{
+		long length = size(0);
+		long stride = this.stride(0);
+
+		T[] result = new T[length];
+		T* ptr = (T*)data();
+		if (stride == 1)
+		{
+			fixed (T* pResult = result)
+			{
+				long byteLength = length * sizeof(T);
+				Buffer.MemoryCopy(ptr, pResult, byteLength, byteLength);
+			}
+		}
+		else
+		{
+			for (long i0 = 0, off0 = 0; i0 < length; i0++, off0 += stride)
+			{
+				result[i0] = ptr[off0];
+			}
+		}
+		return result;
+	}
+
+	public T[,] ToArray2D<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray2D<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(2);
+		return ToArray2D_Internal<T>();
+	}
+
+	private unsafe T[,] ToArray2D_Internal<T>() where T : unmanaged
+	{
+		long length0 = size(0);
+		long length1 = size(1);
+		long stride0 = stride(0);
+		long stride1 = stride(1);
+
+		T[,] result = new T[length0, length1];
+		T* ptr = (T*)data();
+		for (long i0 = 0, off0 = 0; i0 < length0; i0++, off0 += stride0)
+		{
+			for (long i1 = 0, off1 = 0; i1 < length1; i1++, off1 += stride1)
+			{
+				result[i0, i1] = ptr[off0 + off1];
+			}
+		}
+		return result;
+	}
+
+	public T[,,] ToArray3D<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray3D<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(3);
+		return ToArray3D_Internal<T>();
+	}
+
+	private unsafe T[,,] ToArray3D_Internal<T>() where T : unmanaged
+	{
+		long length0 = size(0);
+		long length1 = size(1);
+		long length2 = size(2);
+		long stride0 = stride(0);
+		long stride1 = stride(1);
+		long stride2 = stride(2);
+		T[,,] result = new T[length0, length1, length2];
+		T* ptr = (T*)data();
+		for (long i0 = 0, off0 = 0; i0 < length0; i0++, off0 += stride0)
+		{
+			for (long i1 = 0, off1 = 0; i1 < length1; i1++, off1 += stride1)
+			{
+				for (long i2 = 0, off2 = 0; i2 < length2; i2++, off2 += stride2)
+				{
+					result[i0, i1, i2] = ptr[off0 + off1 + off2];
+				}
+			}
+		}
+		return result;
+	}
+
+	public T[,,,] ToArray4D<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray4D<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(4);
+		return ToArray4D_Internal<T>();
+	}
+
+	private unsafe T[,,,] ToArray4D_Internal<T>() where T : unmanaged
+	{
+		long length0 = size(0);
+		long length1 = size(1);
+		long length2 = size(2);
+		long length3 = size(3);
+		long stride0 = stride(0);
+		long stride1 = stride(1);
+		long stride2 = stride(2);
+		long stride3 = stride(3);
+		T[,,,] result = new T[length0, length1, length2, length3];
+		T* ptr = (T*)data();
+		for (long i0 = 0, off0 = 0; i0 < length0; i0++, off0 += stride0)
+		{
+			for (long i1 = 0, off1 = 0; i1 < length1; i1++, off1 += stride1)
+			{
+				for (long i2 = 0, off2 = 0; i2 < length2; i2++, off2 += stride2)
+				{
+					for (long i3 = 0, off3 = 0; i3 < length3; i3++, off3 += stride3)
+					{
+						result[i0, i1, i2, i3] = ptr[off0 + off1 + off2 + off3];
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	public T[,,,,] ToArray5D<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray5D<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(5);
+		return ToArray5D_Internal<T>();
+	}
+
+	private unsafe T[,,,,] ToArray5D_Internal<T>() where T : unmanaged
+	{
+		long length0 = size(0);
+		long length1 = size(1);
+		long length2 = size(2);
+		long length3 = size(3);
+		long length4 = size(4);
+		long stride0 = stride(0);
+		long stride1 = stride(1);
+		long stride2 = stride(2);
+		long stride3 = stride(3);
+		long stride4 = stride(4);
+		T[,,,,] result = new T[length0, length1, length2, length3, length4];
+		T* ptr = (T*)data();
+		for (long i0 = 0, off0 = 0; i0 < length0; i0++, off0 += stride0)
+		{
+			for (long i1 = 0, off1 = 0; i1 < length1; i1++, off1 += stride1)
+			{
+				for (long i2 = 0, off2 = 0; i2 < length2; i2++, off2 += stride2)
+				{
+					for (long i3 = 0, off3 = 0; i3 < length3; i3++, off3 += stride3)
+					{
+						for (long i4 = 0, off4 = 0; i4 < length4; i4++, off4 += stride4)
+						{
+							result[i0, i1, i2, i3, i4] = ptr[off0 + off1 + off2 + off3 + off4];
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	public T[,,,,,] ToArray6D<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArray6D<T>();
+		}
+		ValidateType<T>();
+		ValidateDimension(6);
+		return ToArray6D_Internal<T>();
+	}
+
+	private unsafe T[,,,,,] ToArray6D_Internal<T>() where T : unmanaged
+	{
+		long length0 = size(0);
+		long length1 = size(1);
+		long length2 = size(2);
+		long length3 = size(3);
+		long length4 = size(4);
+		long length5 = size(5);
+		long stride0 = stride(0);
+		long stride1 = stride(1);
+		long stride2 = stride(2);
+		long stride3 = stride(3);
+		long stride4 = stride(4);
+		long stride5 = stride(5);
+		T[,,,,,] result = new T[length0, length1, length2, length3, length4, length5];
+		T* ptr = (T*)data();
+		for (long i0 = 0, off0 = 0; i0 < length0; i0++, off0 += stride0)
+		{
+			for (long i1 = 0, off1 = 0; i1 < length1; i1++, off1 += stride1)
+			{
+				for (long i2 = 0, off2 = 0; i2 < length2; i2++, off2 += stride2)
+				{
+					for (long i3 = 0, off3 = 0; i3 < length3; i3++, off3 += stride3)
+					{
+						for (long i4 = 0, off4 = 0; i4 < length4; i4++, off4 += stride4)
+						{
+							for (long i5 = 0, off5 = 0; i5 < length5; i5++, off5 += stride5)
+							{
+								result[i0, i1, i2, i3, i4, i5] = ptr[off0 + off1 + off2 + off3 + off4 + off5];
+							}
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	public Array ToArrayND<T>() where T : unmanaged
+	{
+		if (device_type() != DeviceType.CPU)
+		{
+			using Tensor cpuTensor = cpu();
+			return cpuTensor.ToArrayND<T>();
+		}
+		ValidateType<T>();
+		return ndimension() switch
+		{
+			0 => (T[])[ToValue<T>()],
+			1 => ToArray1D_Internal<T>(),
+			2 => ToArray2D_Internal<T>(),
+			3 => ToArray3D_Internal<T>(),
+			4 => ToArray4D_Internal<T>(),
+			5 => ToArray5D_Internal<T>(),
+			6 => ToArray6D_Internal<T>(),
+			_ => ToArrayND_Internal<T>(),
+		};
+	}
+
+	[UnconditionalSuppressMessage("AotAnalysis", "IL3050:RequiresDynamicCode", Justification = "MDArrays of Rank != 1 can be created because they don't implement generic interfaces.")]
+	private unsafe Array ToArrayND_Internal<T>() where T : unmanaged
+	{
+		// Iterate over every index tuple in the N-dimensional space:
+		// - Use an odometer-style index array `indices` initialized to zeros.
+		// - For each position, compute the flat offset into `ptr` as:
+		//   offset = sum(indices[i] * strides[i]) for i in [0..rank-1]
+		// - Read the value from `ptr[offset]` and store it into `result` at `indices`.
+		// - Increment `indices` like an odometer; when the most significant digit rolls over,
+		//   the iteration is complete.
+
+		// Note: Boxing of T will occur when setting values into the Array.
+
+		long[] lengths = sizes();
+		long[] strides = this.strides();
+		Debug.Assert(lengths.Length == strides.Length);
+		Array result = Array.CreateInstance(typeof(T), lengths);
+
+		T* ptr = (T*)data();
+
+		int rank = lengths.Length;
+		Debug.Assert(rank >= 1);
+
+		long[] indices = new long[rank];
+		while (true)
+		{
+			long offset = 0;
+			for (int d = 0; d < rank; d++)
+			{
+				offset += indices[d] * strides[d];
+			}
+
+			T value = ptr[offset];
+			result.SetValue(value, indices);
+
+			// Increment indices odometer-style
+			for (int dim = rank - 1; dim >= 0; dim--)
+			{
+				indices[dim]++;
+				if (indices[dim] < lengths[dim])
+				{
+					break;
+				}
+				indices[dim] = 0;
+				if (dim == 0)
+				{
+					// Completed all iterations
+					return result;
+				}
+			}
+		}
+	}
+
+	public unsafe T ToValue<T>() where T : unmanaged
+	{
+		ValidateType<T>();
+		T* ptr = (T*)data();
+		return *ptr;
+	}
+
+	private void ValidateType<T>()
+	{
+		if (Type != ScalarType.Get<T>())
+		{
+			throw new ArgumentException($"{typeof(T).Name} is not compatible with {Type}");
+		}
+	}
+
+	private void ValidateDimension(int expectedDimension)
+	{
+		if (ndimension() != expectedDimension)
+		{
+			throw new InvalidOperationException($"{ndimension()}-dimensional tensors cannot be converted to {expectedDimension}-dimensional arrays.");
+		}
+	}
+}
--- AssetRipper.Bindings.LibTorchSharp/Tensor.cs
+++ AssetRipper.Bindings.LibTorchSharp/Tensor.cs
@@ -6,61 +6,8 @@ namespace AssetRipper.Bindings.LibTorchSharp;
 [OpaqueStruct(nameof(OpaqueTensor))]
 public readonly partial struct Tensor
 {
-	public unsafe T[] ToArray<T>() where T : unmanaged
-	{
-		if (device_type() != (int)DeviceType.CPU)
-		{
-			using Tensor cpuTensor = cpu();
-			return cpuTensor.ToArray<T>();
-		}
-
-		ValidateType<T>();
-
-		if (ndimension() != 1)
-		{
-			throw new InvalidOperationException("Only 1-dimensional tensors can be converted to arrays.");
-		}
-
-		long length = sizes()[0];
-		long stride = strides()[0];
-
-		T[] result = new T[length];
-		T* ptr = (T*)data();
-		if (stride == 1)
-		{
-			fixed (T* pResult = result)
-			{
-				long byteLength = length * sizeof(T);
-				Buffer.MemoryCopy(ptr, pResult, byteLength, byteLength);
-			}
-		}
-		else
-		{
-			for (long i0 = 0, off0 = 0; i0 < length; i0++, off0 += stride)
-			{
-				result[i0] = ptr[off0];
-			}
-		}
-		return result;
-	}
-
-	public unsafe T ToValue<T>() where T : unmanaged
-	{
-		ValidateType<T>();
-		T* ptr = (T*)data();
-		return *ptr;
-	}
-
 	public Device GetDevice()
 	{
 		return new Device(device_type(), device_index());
-	}
-
-	private void ValidateType<T>()
-	{
-		if (Type != ScalarType.Get<T>())
-		{
-			throw new ArgumentException($"{typeof(T).Name} is not compatible with {Type}");
-		}
 	}
 }
