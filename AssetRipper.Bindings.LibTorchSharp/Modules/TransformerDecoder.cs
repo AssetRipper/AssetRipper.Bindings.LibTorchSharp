4f9f6c2f624ba8a9fdfc4046f623307390db1816
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-09 19:17:29 -0800

    Add more constructors for TransformerDecoder and TransformerEncoder

---
+++ AssetRipper.Bindings.LibTorchSharp/Modules/TransformerDecoder.cs
@@ -0,0 +1,14 @@
+﻿namespace AssetRipper.Bindings.LibTorchSharp.Modules;
+
+public readonly partial struct TransformerDecoder
+{
+	public unsafe TransformerDecoder(long num_layers, long d_model, long nhead, long dim_feedforward, double dropout, long activation)
+	{
+		using TransformerDecoderLayer decoder_layer = new(d_model, nhead, dim_feedforward, dropout, activation);
+		handle = Create(decoder_layer, num_layers, null);
+	}
+
+	public TransformerDecoder(long num_layers, TransformerDecoderLayer decoder_layer) : this(decoder_layer, num_layers)
+	{
+	}
+}
---
+++ AssetRipper.Bindings.LibTorchSharp/Modules/TransformerEncoder.cs
@@ -0,0 +1,14 @@
+﻿namespace AssetRipper.Bindings.LibTorchSharp.Modules;
+
+public readonly partial struct TransformerEncoder
+{
+	public unsafe TransformerEncoder(long num_layers, long d_model, long nhead, long dim_feedforward, double dropout, long activation)
+	{
+		using TransformerEncoderLayer encoder_layer = new(d_model, nhead, dim_feedforward, dropout, activation);
+		handle = Create(encoder_layer, num_layers, null);
+	}
+
+	public TransformerEncoder(long num_layers, TransformerEncoderLayer encoder_layer) : this(encoder_layer, num_layers)
+	{
+	}
+}
