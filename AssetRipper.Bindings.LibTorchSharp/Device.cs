41b56c03664a990de18c5a650563b90d926f7f8b
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-10 20:42:22 -0800

    Fix issue with cuda not being initialized

--- AssetRipper.Bindings.LibTorchSharp/Device.cs
+++ AssetRipper.Bindings.LibTorchSharp/Device.cs
@@ -2,7 +2,33 @@
 
 public record struct Device(DeviceType Type, int Index = -1)
 {
+	static Device()
+	{
+		InitializeDeviceType(DeviceType.CPU);
+	}
+
 	public static Device CPU => new(DeviceType.CPU);
 	public static Device Cuda => new(DeviceType.CUDA);
-	public static Device Default { get; set; } = CPU;
+	public static Device Default
+	{
+		get;
+		set
+		{
+			if (field != value)
+			{
+				InitializeDeviceType(value.Type);
+				field = value;
+			}
+		}
+	} = CPU;
+
+	public readonly void Initialize()
+	{
+		InitializeDeviceType(Type);
+	}
+
+	private static void InitializeDeviceType(DeviceType deviceType)
+	{
+		TorchSharp.torch.InitializeDeviceType((TorchSharp.DeviceType)deviceType);
+	}
 }
