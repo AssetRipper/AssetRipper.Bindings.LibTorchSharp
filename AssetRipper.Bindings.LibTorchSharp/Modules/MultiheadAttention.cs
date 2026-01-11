9b86e5efbb83e4a25ce120e51868d098a36317f4
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-01-11 02:36:26 -0800

    Add overload to MultiheadAttention.Forward

---
+++ AssetRipper.Bindings.LibTorchSharp/Modules/MultiheadAttention.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace AssetRipper.Bindings.LibTorchSharp.Modules;
+
+public readonly partial struct MultiheadAttention
+{
+	[DebuggerHidden]
+	[DebuggerNonUserCode]
+	[DebuggerStepThrough]
+	[StackTraceHidden]
+	public Tensor Forward(Tensor query, Tensor key, Tensor value, Tensor key_padding_mask, Tensor attn_mask)
+	{
+		Forward(query, key, value, key_padding_mask, false, attn_mask, out Tensor result, out Tensor weights);
+		Debug.Assert(weights.IsNull);
+		return result;
+	}
+}
